bac5047442ec19a625b37a8d53988ca1a4755d81
Subramanya <subramanya.adiga4@protonmail.com>
2026-04-05 15:56:05 +0530

    Removed Dependency On VCPKG

--- scripts/generate_msvc_ninja.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mkdir ..\build\standalone
-cd ..\build\standalone
-cmake ../.. -G"Ninja Multi-Config" -DVCPKG_TARGET_TRIPLET=x64-windows-static-md -DCMAKE_TOOLCHAIN_FILE=c:/vcpkg/scripts/buildsystems/vcpkg.cmake
